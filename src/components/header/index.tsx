12e4d38cd20bd1e290937561a9d8c9668c431635
Victor Hugo <vt.hugo.2021@gmail.com>
2023-07-30 16:13:55 -0300

    feature: animate the user menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,11 +4,12 @@ import { CaretDown, CaretUp } from '@phosphor-icons/react'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { UserProps } from '@/utils/types/dashboard'
 import Link from 'next/link'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const Header = ({ user }: UserProps) => {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <header className="fixed left-0 top-0 flex w-screen justify-end border-[1px] border-slate-300 py-3 pr-4">
+    <header className="fixed left-0 top-0 flex w-screen justify-end border-[1px] border-slate-300 py-5 pr-12">
       <DropdownMenu.Root onOpenChange={setIsOpen}>
         <DropdownMenu.Trigger asChild>
           <div className="flex cursor-pointer items-center gap-2">
@@ -38,23 +39,35 @@ export const Header = ({ user }: UserProps) => {
             </span>
           </div>
         </DropdownMenu.Trigger>
+        <AnimatePresence>
+          <DropdownMenu.Portal>
+            {isOpen && (
+              <DropdownMenu.Content
+                forceMount
+                className=" mr-6 min-w-[220px] rounded-md bg-slate-100 p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform]"
+                sideOffset={10}
+              >
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.8 }}
+                >
+                  <DropdownMenu.Item className="relative flex h-[25px] cursor-pointer select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none hover:bg-violet-main hover:text-white">
+                    My Profile
+                  </DropdownMenu.Item>
 
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content
-            className=" mr-6 min-w-[220px] rounded-md bg-slate-100 p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform]"
-            sideOffset={10}
-          >
-            <DropdownMenu.Item className="relative flex h-[25px] cursor-pointer select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none hover:bg-violet-main hover:text-white">
-              My Profile
-            </DropdownMenu.Item>
+                  <Link href="/api/auth/logout">
+                    <DropdownMenu.Item className="relative flex h-[25px] cursor-pointer select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none hover:bg-violet-main hover:text-red-400 data-[disabled]:pointer-events-none">
+                      Exit
+                    </DropdownMenu.Item>
+                  </Link>
 
-            <DropdownMenu.Item className="relative flex h-[25px] cursor-pointer select-none items-center rounded-[3px] px-[5px] pl-[25px] text-[13px] leading-none outline-none hover:bg-violet-main hover:text-red-600 data-[disabled]:pointer-events-none">
-              <Link href="/api/auth/logout">Exit</Link>
-            </DropdownMenu.Item>
-
-            <DropdownMenu.Arrow className="fill-white" />
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
+                  <DropdownMenu.Arrow className="fill-white" />
+                </motion.div>
+              </DropdownMenu.Content>
+            )}
+          </DropdownMenu.Portal>
+        </AnimatePresence>
       </DropdownMenu.Root>
     </header>
   )

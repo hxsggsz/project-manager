32d7b906c3dfc835e2527d0ce3a0890d899084a1
Victor Hugo <vt.hugo.2021@gmail.com>
2023-08-05 01:02:32 -0300

    add tests for auth button component and update button test

---
+++ src/components/auth-buttons/index.spec.tsx
@@ -0,0 +1,14 @@
+import { AuthButtons } from '.'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+describe('AuthButtons', () => {
+  it('should render and call the button correctly', () => {
+    render(<AuthButtons href="test-href">test</AuthButtons>)
+    const button = screen.getByText('test')
+    userEvent.click(button)
+
+    expect(button).toBeInTheDocument()
+    expect(button).toHaveAttribute('href', 'test-href')
+  })
+})
--- src/components/button/index.spec.tsx
+++ src/components/button/index.spec.tsx
@@ -1,16 +1,27 @@
 import { Button } from '.'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('Button', () => {
   it('should render the button content', () => {
-    render(<Button>teste</Button>)
-    const testButton = screen.getByText('teste')
+    render(<Button>test</Button>)
+    const testButton = screen.getByText('test')
     expect(testButton).toBeVisible()
   })
 
   it('should render the loading button', () => {
-    render(<Button isLoading>teste</Button>)
+    render(<Button isLoading>test</Button>)
     const loading = screen.getByTestId('loading')
+    const buttonText = screen.queryByText('test')
     expect(loading).toBeVisible()
+    expect(buttonText).not.toBeInTheDocument()
+  })
+
+  it('should click the button and dispare click event', async () => {
+    const handleClick = jest.fn()
+    render(<Button onClick={handleClick}>test</Button>)
+    const buttonText = screen.getByText('test')
+    userEvent.click(buttonText)
+    await waitFor(() => expect(handleClick).toHaveBeenCalled())
   })
 })
